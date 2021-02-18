fcbe0902067c5d8bc34a89d936906bcd45a9a0e5
yoshiweb <yoshiweb@gmail.com>
2021-02-18 18:41:59 +0900

    feat: tensorflow keras upgrade
    #4

--- keras-mnist/mnist_mlp/mnist_mlp_predict.py
+++ keras-mnist/mnist_mlp/mnist_mlp_predict.py
@@ -16,7 +16,7 @@ filename = sys.argv[1]
 
 
 # 画像を読み込んで4次元テンソルへ変換
-img = image.load_img(filename, target_size=(img_height, img_width), grayscale=True)
+img = image.load_img(filename, target_size=(img_height, img_width), color_mode='grayscale')
 x = image.img_to_array(img)
 x = np.expand_dims(x, axis=0)
 
